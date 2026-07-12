994a0faa533ea3a3170058125c51c87134eb9195
Jorge <jorgeizarra26@gmail.com>
2026-07-11 23:36:04 -0500

    feat(api): POST /guardar con folio, campos completos y RN-04 (RF-07, RF-08, RN-04, SPEC-API-07 parcial)

--- src/interfaces/routes/formularios.routes.ts
+++ src/interfaces/routes/formularios.routes.ts
@@ -3,6 +3,7 @@ import { putIdentificacion } from '../controllers/identificacion.controller';
 import { putContacto } from '../controllers/contacto.controller';
 import { putPerfilEconomico } from '../controllers/perfil-economico.controller';
 import { postDocumento } from '../controllers/documento.controller';
+import { postGuardar } from '../controllers/guardar.controller';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.put('/:id/identificacion', putIdentificacion);
 router.put('/:id/contacto', putContacto);
 router.put('/:id/perfil-economico', putPerfilEconomico);
 router.post('/:id/documentos', postDocumento);
+router.post('/:id/guardar', postGuardar);
 
 export { router as formulariosRouter };
